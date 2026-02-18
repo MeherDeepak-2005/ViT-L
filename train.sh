dc65343e703880123ca85284910d9ee838917ff9
MeherDeepak-2005 <meherdeepakpc@gmail.com>
2026-02-18 16:33:05 +0530

    Cloud training

---
+++ train.sh
@@ -0,0 +1,18 @@
+echo "Starting Setup"
+
+echo "update apt"
+apt update
+
+echo "install unzip"
+apt install -y unzip
+
+echo "installing pandas tqdm"
+pip install -y pandas tqdm
+
+echo "install data files"
+wget -P ~/coner-vision -O ~/coner-vision/data.zip "https://drivendata-prod.s3.amazonaws.com/data/87/public/competition_VfIpjyh.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIARVBOBDCYSN7TAHVS%2F20260218%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20260218T092425Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=e706f7852fbeb5c71ecf0c714855ebe39d50471a64460ab610d26c6aa68cc3af"
+mkdir ~/coner-vision/data
+unzip ~/coner-vision/data.zip -d ~/coner-vision/data
+
+
+python model.py --data_dir=./data batch_size=128
